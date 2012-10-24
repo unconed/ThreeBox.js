c62ac1b17a7176289907c2453b0e477219ba20d6
Steven Wittens <steven@acko.net>
2012-10-24 08:57:46 -0700

    extended builds

--- build.sh
+++ build.sh
@@ -11,11 +11,18 @@ src/Preload.js
 VENDOR="
 vendor/microevent.js
 vendor/microajax.js
+vendor/underscore.js
 "
 
 cat $VENDOR $SRC > build/ThreeBox.js
+cat $SRC > build/ThreeBox-core.js
 
 curl --data-urlencode "js_code@build/ThreeBox.js" 	\
 	-d "output_format=text&output_info=compiled_code&compilation_level=SIMPLE_OPTIMIZATIONS" \
 	http://closure-compiler.appspot.com/compile	\
 	> build/ThreeBox.min.js
+
+curl --data-urlencode "js_code@build/ThreeBox-core.js" 	\
+	-d "output_format=text&output_info=compiled_code&compilation_level=SIMPLE_OPTIMIZATIONS" \
+	http://closure-compiler.appspot.com/compile	\
+	> build/ThreeBox-core.min.js
